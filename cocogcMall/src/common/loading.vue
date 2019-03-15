96e9cb04ad3b1f09c927225869f08c482c94ae1f
front <front123>
2019-03-15 11:12:48 +0800

    列表排序，增加首页

---
+++ cocogcMall/src/common/loading.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="loading" id="moreMsg"  >
+        <div class="loading-box tc" v-if="isLoading">
+            <mt-spinner type="snake" class="loading-more"></mt-spinner>
+            <span class="loading-more-txt">加载中...</span>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data:{
+      return:{
+          isLoading:true,
+         
+      }  
+    },
+    mounted() {
+        var _this = this
+        this.observer = new IntersectionObserver(([entry]) => {
+            console.log(entry.target.id)
+            if(entry.target.id == 'moreMsg'){
+                _this.isLoading = true;
+            }
+        })
+        this.observer.observe(this.$el);
+        
+    }
+
+}
+</script>
+<style lang="less" scoped>
+
+</style>
