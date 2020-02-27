dbeb7f2c020c1843d232bc7086cb4b980bdc33bc
xfFelix <13590344852@163.com>
2020-02-27 11:18:05 +0800

    新增百度统计

---
+++ cocogcMall/src/assets/js/hmt.js
@@ -0,0 +1,7 @@
+let _hmt = _hmt || [];
+(function() {
+  let hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?493589a819073b03d72b6a9f1693de58";
+  let s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
